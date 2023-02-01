8b7bc01c70396bf8ac55e7df6a5e78838099968a
bhar75 <bharathi_n@msn.com>
2023-02-01 22:44:02 +0530

    complete app with navigator and clipboard property, and values.js package for the color generation

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, hex, index, type }) => {
+  const [alert, setAlert] = useState(false);
+  const backColor = rgb.join(",");
 
-export default SingleColor
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  const handleClick = () => {
+    setAlert(true);
+    navigator.clipboard.writeText("#" + hex);
+  };
+
+  return (
+    <article
+      className={`color ${index > 10 && `color-light`}`}
+      style={{ background: `rgb(${backColor})` }}
+      onClick={handleClick}
+    >
+      <p className="percent-value">{weight}</p>
+      <p className="color-value">#{hex}</p>
+      <p className="color-value">
+        <u>{type}</u>
+      </p>
+      {alert && <p>copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
